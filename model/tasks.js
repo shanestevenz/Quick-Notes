80c40fa9a2e3cf27490d9aff023184b843c3c251
Siddhartha PRadhan <sppradhan@wpi.edu>
2022-10-11 15:48:40 -0400

    added mongoose files

--- model/tasks.js
+++ model/tasks.js
@@ -3,39 +3,40 @@ require('dotenv').config() // TODO: Remove in production
 const Schema = mongoose.Schema
 const ObjectId = require('mongodb').ObjectId
 
-// app data is {"task":"Task Name",   "dueDate": "When the task is due", "taskType":"Work or personal"}
-// self generated: {"taskID" : "Unique ID for a task", "taskCreationTime": "Based on when the request was made", "taskUrgency": "How urgent the Task is, based on (dueDate - taskCreationDate)"}
-
-const uri = 'mongodb+srv://'+process.env.MONGO_USER+':'+process.env.MONGO_PASS+'@'+process.env.MONGO_HOST+"/DoIt"
+const uri = 'mongodb+srv://'+process.env.MONGO_USER+':'+process.env.MONGO_PASS+'@'+process.env.MONGO_HOST+"/QuickNotes"
 // connect Mongoose
 mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology:true})
 
-const Task = new Schema({
-    task: {
+const Note = new Schema({
+    noteTitle: {
         type: String,
         required: true
     },
-    dueDate: {
+    noteContent: {
         type: String,
         optional: true
     },
-    taskType:{
+    noteColor:{
         type: String,
         required:true
     },
-    taskCreationTime:{
+    posX:{
         type: String,
         required:true
     },
-    taskUrgency:{
+    poxY:{
         type: String,
         optional: true
     },
     userId:{
         type:ObjectId,
         required:true
+    }, 
+    boardId:{
+        type:Number,
+        required:true
     }
 
 })
 
-module.exports = mongoose.model('Tasks',Task)
+module.exports = mongoose.model('Notes',Note)
