d2f340f31054e9adb143d1a6e7318fcdd27eab12
Siddhartha PRadhan <sppradhan@wpi.edu>
2022-10-16 22:58:13 -0400

    Updated login and register UI, added bg to notes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -43,16 +43,21 @@ export default function Login() {
   };
 
   return (
-    <div className="bg-gray-900 text-white text-opacity-80 flex justify-center items-center h-screen w-screen">
-      <form className="min-w-[20%]" onSubmit={handleSubmit}>
-        <div className="login-wrapper m-auto flex flex-col  border rounded-lg p-5 bg-slate-800 border-yellow-600">
-          <p className="my-3 mb-5 font-bold text-3xl">Sign in</p>
+    <div className="bg-stone-900 text-white text-opacity-80 flex justify-center items-center h-screen w-screen ">
+      <form className="min-w-[20%] " onSubmit={handleSubmit}>
+        <div className="login-wrapper m-auto flex flex-col  rounded-lg bg-zinc-800 px-16 py-16 ">
+          <div className="flex flex-row">
+            <p className="mb-5 font-semibold text-3xl ">
+              Sign into
+              <p className="text-emerald-500 font-bold">Quick Notes</p>
+            </p>
+          </div>
           <p className="mb-2 text-sm text-slate-300 dark:text-white font-normal">
             Username
           </p>
 
           <input
-            className="mb-5 p-2 bg-inherit border border-yellow-600 rounded-lg focus-visible:border-blue-900"
+            className="mb-5 p-2 bg-inherit border border-b rounded-lg focus-visible:border-transparent"
             type="text"
             placeholder="Enter Username"
             onChange={(e) => setUserName(e.target.value)}
@@ -62,29 +67,29 @@ export default function Login() {
             Password
           </p>
           <input
-            className="mb-5 p-2 bg-inherit border border-yellow-600 rounded-lg focus-visible:border-blue-900"
+            className="mb-5 p-2 bg-inherit border border-blue-gray-700 rounded-lg focus-visible:border-transparent"
             type="password"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
           />
 
           <button
-            className="py-2 mb-3 bg-transparent hover:bg-yellow-600 border  border-yellow-600 hover:border-transparent text-white font-normal rounded"
+            className="py-2 mb-3 bg-transparent hover:bg-emerald-500 border border-emerald-500 hover:border-transparent text-white font-normal rounded"
             type="submit"
           >
-            Submit
+            Log in
           </button>
           <a
-            className="text-center button py-2 bg-slate-200 hover:bg-yellow-600 hover:text-white border border-black hover:border-transparent text-black font-semibold rounded"
+            className="text-center button py-2 bg-transparent hover:bg-gray-400 text-white border border-gray-400 hover:border-transparent hover:text-black font-semibold rounded"
             href="/auth/github"
             onClick={(e) => authenticateWithGithub(e)}
           >
-            Sign in with GitHub
+            Continue with GitHub
           </a>
           <p className="text-grey-700 mt-4 text-center">or</p>
 
           <Link
-            className="text-center mx-auto text-opacity-70 hover:text-opacity-90 text-yellow-500 font-light underline hover:decoration-wavy decoration-yellow-500 hover:underline-offset-4 w-16"
+            className="text-center mx-auto text-opacity-70 hover:text-opacity-90 text-emerald-500 font-light underline hover:decoration-wavy decoration-emerald-500 hover:underline-offset-4 w-16"
             to="/register"
           >
             Register
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 async function registerUser(credentials) {
   const bodyVal = new URLSearchParams();
@@ -14,6 +14,8 @@ async function registerUser(credentials) {
 }
 
 export default function Register() {
+  const navigate = useNavigate();
+
   //add some kind of token
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
@@ -26,6 +28,7 @@ export default function Register() {
     console.log(registerResponse);
     if (registerResponse.ok) {
       // reroute to login screen after a timeout
+      navigate("/", { replace: true }); // redirect to login
       console.log("Succesfully registered user.");
     } else if (registerResponse.status === 401) {
       console.log("Username exists.");
@@ -34,17 +37,27 @@ export default function Register() {
     }
   };
 
+  function authenticateWithGithub(e) {
+    e.preventDefault();
+    window.open("http://localhost:3000/auth/github", "_self");
+  }
+
   return (
-    <div className="bg-gray-900 text-white text-opacity-80 flex justify-center items-center h-screen w-screen">
+    <div className="bg-stone-900 text-white text-opacity-80 flex justify-center items-center h-screen w-screen ">
       <form className="min-w-[20%]" onSubmit={handleSubmit}>
-        <div className="login-wrapper m-auto  flex flex-col justify-center border rounded-lg p-5 bg-slate-800 border-yellow-600">
-          <p className="my-3 mb-5 font-bold text-3xl">Register</p>
+        <div className="login-wrapper m-auto flex flex-col  rounded-lg bg-zinc-800 px-16 py-16 ">
+          <div className="flex flex-row">
+            <p className="mb-5 font-semibold text-3xl">
+              Register for{" "}
+              <p className="text-emerald-500 font-bold">Quick Notes</p>
+            </p>
+          </div>
           <p className="mb-2 text-sm text-slate-300 dark:text-white font-normal">
             Username
           </p>
 
           <input
-            className="mb-5 p-2 bg-inherit border border-yellow-600 rounded-lg focus-visible:border-blue-900"
+            className="mb-5 p-2 bg-inherit border border-b rounded-lg focus-visible:border-transparent"
             type="text"
             placeholder="Enter Username"
             onChange={(e) => setUserName(e.target.value)}
@@ -54,28 +67,29 @@ export default function Register() {
             Password
           </p>
           <input
-            className="mb-5 p-2 bg-inherit border border-yellow-600 rounded-lg focus-visible:border-blue-900"
+            className="mb-5 p-2 bg-inherit border border-blue-gray-700 rounded-lg focus-visible:border-transparent"
             type="password"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
           />
 
           <button
-            className="py-2 mb-3 bg-transparent hover:bg-yellow-600 border  border-yellow-600 hover:border-transparent text-white font-normal rounded"
+            className="py-2 mb-3 bg-transparent hover:bg-emerald-500 border border-emerald-500 hover:border-transparent text-white font-normal rounded"
             type="submit"
           >
             Register
           </button>
-          <button
-            className="text-center button py-2 bg-slate-200 hover:bg-yellow-600 hover:text-white border border-black hover:border-transparent text-black font-semibold rounded"
+          <a
+            className="text-center button py-2 bg-transparent hover:bg-gray-400 text-white border border-gray-400 hover:border-transparent hover:text-black font-semibold rounded"
             href="/auth/github"
+            onClick={(e) => authenticateWithGithub(e)}
           >
-            Sign in with GitHub Instead
-          </button>
+            Sign in with GitHub
+          </a>
           <p className="text-grey-700 mt-4 text-center">or</p>
 
           <Link
-            className="text-center mx-auto text-opacity-70 hover:text-opacity-90 text-yellow-500 font-light underline hover:decoration-wavy decoration-yellow-500 hover:underline-offset-4 w-16"
+            className="text-center mx-auto text-opacity-70 hover:text-opacity-90 text-emerald-500 font-light underline hover:decoration-wavy decoration-emerald-500 hover:underline-offset-4 w-16"
             to="/"
           >
             Sign in
